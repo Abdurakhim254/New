1b544dcadef9d308981c1064310437926da14579
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2024-11-21 14:59:58 +0500

    biroz o'zgartirishlar kiritildi

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,20 +1,30 @@
-import { pool } from '../Database/index.js'
+import { logger } from '../utils/logger.js'
 
 export const getCategoryes = async () => {
-    const result = await knex('category').select('*')
-    if (result.rows.length >= 1) {
-        return result.rows
-    } else {
-        return 'Categorylar topilmadi'
+    try {
+        const result = await knex('category').select('*')
+        if (result.rows.length >= 1) {
+            return result.rows
+        } else {
+            return 'Categorylar topilmadi'
+        }
+    } catch (error) {
+        logger.error(error)
+        return error
     }
 }
 
 export const getcategorybyid = async (id) => {
-    const result = await knex('category').where({ id }).first()
-    if (result.rows.length >= 1) {
-        return result.rows
-    } else {
-        return 'Category topilmadi'
+    try {
+        const result = await knex('category').where({ id }).first()
+        if (result.rows.length >= 1) {
+            return result.rows
+        } else {
+            return 'Category topilmadi'
+        }
+    } catch (error) {
+        logger.error(error)
+        return error
     }
 }
 
@@ -25,24 +35,19 @@ export const createCategory = async ({
     created_at,
     updated_at,
 }) => {
-    if (!created_at || !updated_at) {
-        if (!created_at || !updated_at) {
-            await knex('category').where({ email }).update({
-                name,
-                description,
-                tag,
-            })
-        } else {
-            await knex('category').where({ email }).update({
-                name,
-                description,
-                tag,
-                created_at,
-                updated_at,
-            })
-        }
-        await pool.query(query, arr)
+    try {
+        await knex('category').insert({
+            name,
+            description,
+            tag,
+            created_at,
+            updated_at,
+        })
+
         return 'Category yaratildi'
+    } catch (error) {
+        logger.error(error)
+        return error
     }
 }
 
@@ -54,37 +59,40 @@ export const updatecategory = async ({
     updated_at,
     id,
 }) => {
-    const result = await knex('category').where({ id }).first()
-    if (result.rows.length >= 1) {
-        if (!created_at || !updated_at) {
-            await knex('category').where({ email }).update({
-                name,
-                description,
-                tag,
-                id,
-            })
-        } else {
-            await knex('category').where({ email }).update({
+    try {
+        const result = await knex('category').where({ id }).first()
+
+        if (result.rows.length >= 1) {
+            await knex('category').where({ id }).update({
                 name,
                 description,
                 tag,
                 created_at,
                 updated_at,
-                id,
             })
+
+            return 'Category yangilandi'
+        } else {
+            return 'Yangilanadigan Category topilmadi'
         }
-        return 'Category yangilandi'
-    } else {
-        return 'Yangilanadigan Category topilmadi'
+    } catch (error) {
+        logger.error(error)
+        return error
     }
 }
 
 export const deletecategory = async (id) => {
-    const result = await knex('category').where({ id }).first()
-    if (result.rows.length >= 1) {
-        await pool.query(deletequery, [id])
-        return "Category o'chirildi"
-    } else {
+    try {
+        const result = await knex('category').where({ id }).first()
+
+        if (result.rows[0]) {
+            await knex('category').where({ id }).del()
+            return "Category o'chirildi"
+        }
+
         return "O'chiriladigan Category topilmadi"
+    } catch (error) {
+        logger.error(error)
+        return error
     }
 }
